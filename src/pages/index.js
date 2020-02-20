245b82f8bbc06e6d28e52a730ff525aa719df1ee
Matteo Franzoi <matteo@DESKTOP-ESMTFMM.localdomain>
2020-02-20 14:21:46 +0100

    Add cookie alert

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,10 @@ import About from 'components/about'
 import Work from 'components/work'
 import Other from 'components/other'
 import Writing from 'components/writing'
-import Reading from 'components/reading'
+//import Reading from 'components/reading'
 import Footer from 'components/footer'
+import CookieConsent from "react-cookie-consent";
+
 
 //import DarkModeToggle from 'components/handleTheme'
 
@@ -35,7 +37,21 @@ export default class App extends React.Component {
           </Section> */}
         </Section>
         <Footer />
+        <CookieConsent
+          location="top"
+          buttonText="I Accept"
+          cookieName="myCookie"
+          style={{ background: "#3B3E48", fontSize: "16px", color: "#E8EEF2"}}
+          buttonStyle={{ border: "0px", background: "#4EA6A6", fontSize: "14.4px" }}
+          expires={150}
+        >
+          This website uses cookies to enhance the user experience. {" "}
+          <span style={{ fontSize: "12px", color: "#E8EEF2" }}>
+            <a style={{ fontSize: "12px", color: "#E8EEF2" }} href="https://ec.europa.eu/info/cookies_en" target="_blank"> More info</a>
+          </span>
+        </CookieConsent>
       </GlobalWrapper>
     )
   }
 }
+
